48d2fd938d5fc5466cca23479cfa9da01e92f909
DJ Damelo <dj.damelo.music@gmail.com>
2026-07-31 13:15:48 +0000

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,12 +1,1 @@
-export default async function handler(req, res) {
-  // Important pour que Vercel lise le JSON
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
 
-  const { paymentId } = req.body;
-  console.log("APPROUVE:", paymentId);
-
-  // On répond direct à Pi pour qu'il ne bloque pas
-  return res.status(200).json({ status: "approved" });
-}
